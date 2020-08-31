1f973d4af6b188af27435746b9ec0ca1edd3f8cd
Collin Lee <cstlee@cs.stanford.edu>
2020-08-31 16:31:41 -0700

    Fix latency sample dump
    Fix a bug where not all available latency samples are dumped if the
    sample buffer is overwritten.

--- src/DpcBenchmark.cc
+++ src/DpcBenchmark.cc
@@ -178,10 +178,10 @@ DpcBenchmark::dump_stats()
         nlohmann::json client_stats_json;
         client_stats_json["count"] = client_stats.count.load();
         client_stats_json["failures"] = client_stats.failures.load();
-        uint64_t max_sample_index =
-            client_stats.sample_count.load() & SAMPLE_INDEX_MASK;
+        uint64_t const sample_count = std::min(
+            static_cast<uint64_t>(client_stats_json["count"]), MAX_SAMPLES);
         std::vector<uint> latencies;
-        for (uint64_t i = 0; i <= max_sample_index; ++i) {
+        for (uint64_t i = 0; i < sample_count; ++i) {
             latencies.push_back(
                 PerfUtils::Cycles::toNanoseconds(client_stats.samples.at(i)));
         }
